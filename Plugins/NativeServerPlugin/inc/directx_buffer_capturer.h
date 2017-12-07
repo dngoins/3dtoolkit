275d545b083cb512913e3a5f82002412cfe964e1
Phong Cao <phong.caothai@live.com>
2017-12-07 17:24:55 -0500

    Reverted staging buffer optimization due to an issue with VM.

--- Plugins/NativeServerPlugin/inc/directx_buffer_capturer.h
+++ Plugins/NativeServerPlugin/inc/directx_buffer_capturer.h
@@ -16,8 +16,6 @@
 
 #include "buffer_capturer.h"
 
-#define MAX_STAGING_BUFFERS		10
-
 namespace StreamingToolkit
 {
 	// Provides DirectX implementation of the BufferCapturer class.
@@ -33,10 +31,11 @@ namespace StreamingToolkit
 		void SendFrame(ID3D11Texture2D* frame_buffer);
 
 	private:
-		ID3D11Texture2D* UpdateStagingBuffer(ID3D11Texture2D* frame_buffer);
+		void UpdateStagingBuffer(ID3D11Texture2D* frame_buffer);
 
 		Microsoft::WRL::ComPtr<ID3D11Device> d3d_device_;
 		Microsoft::WRL::ComPtr<ID3D11DeviceContext> d3d_context_;
-		ID3D11Texture2D* staging_frame_buffers_[MAX_STAGING_BUFFERS];
+		Microsoft::WRL::ComPtr<ID3D11Texture2D> staging_frame_buffer_;
+		D3D11_TEXTURE2D_DESC staging_frame_buffer_desc_;
 	};
 }
